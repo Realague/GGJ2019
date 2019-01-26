f7233bf42783f47f9b6f06c8ea2cccaf656b4de1
Realague <realague@gmail.com>
2019-01-26 15:33:07 +0100

    Cow

---
+++ GGJ2019/Assets/Scripts/Cow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow : MonoBehaviour
+{
+    private Animator myAnimator;
+    [SerializeField]
+    private float speed = 4f;
+    public int hp = 1;
+
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetFloat("Speed", 1);
+    }
+
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector2(1, 0) * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Cowshed")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
